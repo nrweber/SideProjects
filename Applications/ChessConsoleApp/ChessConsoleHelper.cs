5d1425aff01ffabb9fdb0fa01fdb6c83fb72817b
Nic Weber <nrweber@gmail.com>
2021-12-22 15:29:34 -0600

    add simple chess console app

---
+++ Applications/ChessConsoleApp/ChessConsoleHelper.cs
@@ -0,0 +1,90 @@
+namespace ChessConsoleApp;
+
+public static class ChessConsoleHelper
+{
+    public static Dictionary<PIECE, (char letter, ConsoleColor color)> PieceDisplayInfo = new(){
+        {PIECE.WHITE_PAWN,   ('P', ConsoleColor.Yellow)},
+        {PIECE.WHITE_ROOK,   ('R', ConsoleColor.Yellow)},
+        {PIECE.WHITE_KNIGHT, ('N', ConsoleColor.Yellow)},
+        {PIECE.WHITE_BISHOP, ('B', ConsoleColor.Yellow)},
+        {PIECE.WHITE_QUEEN,  ('Q', ConsoleColor.Yellow)},
+        {PIECE.WHITE_KING,   ('K', ConsoleColor.Yellow)},
+        {PIECE.BLACK_PAWN,   ('p', ConsoleColor.Blue)},
+        {PIECE.BLACK_ROOK,   ('r', ConsoleColor.Blue)},
+        {PIECE.BLACK_KNIGHT, ('n', ConsoleColor.Blue)},
+        {PIECE.BLACK_BISHOP, ('b', ConsoleColor.Blue)},
+        {PIECE.BLACK_QUEEN,  ('q', ConsoleColor.Blue)},
+        {PIECE.BLACK_KING,   ('k', ConsoleColor.Blue)},
+        {PIECE.NONE,         (' ', ConsoleColor.White)}
+    };
+
+    public static void PrintBoard(BoardState state)
+    {
+        if(state.CurrentTurn == PLAYER.WHITE)
+        {
+            for(int r = 7; r >= 0; r--)
+            {
+                Console.WriteLine("   +---+---+---+---+---+---+---+---+");
+                Console.Write($"{r+1}  ");
+                for(int c = 0; c <= 7; c++)
+                {
+                    var displayData = PieceDisplayInfo[state.Board[r,c]];
+                    Console.Write("| ");
+                    Console.ForegroundColor = displayData.color;
+                    Console.Write($"{displayData.letter} ");
+                    Console.ResetColor();
+                }
+                Console.WriteLine("|");
+            }
+            Console.WriteLine("   +---+---+---+---+---+---+---+---+");
+            Console.WriteLine("     a   b   c   d   e   f   g   h  ");
+        }
+        else
+        {
+            for(int r = 0; r <= 7; r++)
+            {
+                Console.WriteLine("   +---+---+---+---+---+---+---+---+");
+                Console.Write($"{r+1}  ");
+                for(int c = 7; c >= 0; c--)
+                {
+                    var displayData = PieceDisplayInfo[state.Board[r,c]];
+                    Console.Write("| ");
+                    Console.ForegroundColor = displayData.color;
+                    Console.Write($"{displayData.letter} ");
+                    Console.ResetColor();
+                }
+                Console.WriteLine("|");
+            }
+            Console.WriteLine("   +---+---+---+---+---+---+---+---+");
+            Console.WriteLine("     h   g   f   e   d   c   b   a  ");
+        }
+    }
+
+    public static string GenerateMoves(BoardState state)
+    {
+        var moves = ChessHelper.PossibleMoves(state);
+
+        StringBuilder output = new();
+        foreach(var m in moves)
+        {
+            output.Append($"{m.Algebraic}\n");
+        }
+        return output.ToString();
+    }
+
+    public static (BoardState newState, string outputText) MakeMove(BoardState state, string moveStr)
+    {
+        List<Move> moves = ChessHelper.PossibleMoves(state).Where(x => x.Algebraic == moveStr).ToList();
+
+        if(moves.Count > 0)
+        {
+            var move = moves[0];
+            var (_, newState) =  ChessHelper.MakeMove(state, move);
+            return (newState, $"Made Move {move.Algebraic}");
+        }
+        else
+        {
+            return (state, "Invalid move");
+        }
+    }
+}
---
+++ Applications/ChessConsoleApp/Program.cs
@@ -0,0 +1,50 @@
+﻿var state = new BoardState();
+
+Console.Clear();
+ChessConsoleHelper.PrintBoard(state);
+Console.WriteLine(ChessHelper.ToFENNotation(state));
+
+bool run = true;
+while(run)
+{
+    Console.Write("command > ");
+    var input = Console.ReadLine();
+
+    if(input == null)
+        continue;
+
+
+    String extraOutputText = "";
+    switch(input)
+    {
+        case "q":
+            run = false;
+            break;
+        case "m":
+            extraOutputText = ChessConsoleHelper.GenerateMoves(state);
+            break;
+        case "":
+            break;
+        default:
+            (state, extraOutputText) = ChessConsoleHelper.MakeMove(state, input);
+            break;
+
+    }
+
+    Console.Clear();
+    ChessConsoleHelper.PrintBoard(state);
+    if(extraOutputText != "")
+        Console.WriteLine(extraOutputText);
+    Console.WriteLine(ChessHelper.ToFENNotation(state));
+}
+
+Console.Clear();
+Console.WriteLine("Thank you for playing!");
+
+
+
+
+
+
+
+
