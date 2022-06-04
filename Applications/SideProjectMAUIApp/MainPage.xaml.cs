14242e70bf6559eb60b0900d23ed487073a21add
Nic Weber <nrweber@gmail.com>
2022-06-04 10:01:35 -0500

    Start MAUI app

---
+++ Applications/SideProjectMAUIApp/MainPage.xaml.cs
@@ -0,0 +1,9 @@
+﻿namespace SideProjectMAUIApp;
+
+public partial class MainPage : ContentPage
+{
+	public MainPage()
+	{
+		InitializeComponent();
+	}
+}
---
+++ Applications/SideProjectMAUIApp/Platforms/iOS/AppDelegate.cs
@@ -0,0 +1,9 @@
+﻿using Foundation;
+
+namespace SideProjectMAUIApp;
+
+[Register("AppDelegate")]
+public class AppDelegate : MauiUIApplicationDelegate
+{
+	protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+}
